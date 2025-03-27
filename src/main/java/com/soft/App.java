9aea6156b4e3efd8040b5207eabc725a5f24b932
Awadhut <awadhutyenkar@gmail.com>
2025-03-28 00:55:22 +0530

    first commit

--- src/main/java/com/soft/App.java
+++ src/main/java/com/soft/App.java
@@ -5,6 +5,8 @@ package com.soft;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+    	System.out.println("Hello World!");
+    	System.out.println("First commit");
+
     }
 }
